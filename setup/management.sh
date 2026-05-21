1d581c5cca30e2c3a48a395ec560450ee58040b4
Troy <tdhayer@tntserver.com>
2026-05-21 11:44:58 -0500

    2fa

--- setup/management.sh
+++ setup/management.sh
@@ -45,7 +45,7 @@ hide_output $venv/bin/pip install --upgrade pip
 hide_output $venv/bin/pip install --upgrade \
 	rtyaml "email_validator>=1.0.0" "exclusiveprocess" \
 	flask dnspython python-dateutil expiringdict gunicorn \
-	qrcode[pil] pyotp \
+	qrcode[pil] pyotp webauthn \
 	"idna>=2.0.0" "cryptography==37.0.2" psutil postfix-mta-sts-resolver \
 	b2sdk boto3
 
